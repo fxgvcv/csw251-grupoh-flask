8f2e927fefa15310ffa791017714b972a5a4fd0e
fxgvcv <gabriel.fogaca@edu.pucrs.br>
2025-06-10 19:01:46 -0300

    feat: Update CI workflow to trigger on develop branch and add configuration classes for environment settings

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import os
+from src.utils.config.config import DevConfig, ProdConfig
 from flask import Flask
 from src.repositories.building_repository import BuildingRepository
 from src.repositories.room_repository import RoomRepository
@@ -10,7 +12,12 @@ from src.models.room_model import Room
 from src.utils.shared.db.base import Base, db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:postgres@sarc-db-instance.cuqc68zbmxj1.us-east-1.rds.amazonaws.com:5432/sarcdb"
+
+if os.getenv("FLASK_ENV") == "development":
+    app.config.from_object(DevConfig)
+else:
+    app.config.from_object(ProdConfig)
+    
 db.init_app(app)
 
 with app.app_context():
